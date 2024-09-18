ca4753efe30d47d42f4c2df6a9f24746cf20552f
Adarsh Jha <adarsh2005j@gmail.com>
2024-09-18 18:29:54 +0530

    yayaya

---
+++ 02_comments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Comments"
+
+#This is a simnle line Comment
+
+<<comment
+This
+is
+Multi
+Line Comments
+comment
---
+++ 03_vardemo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Variables
+
+name="Luffy"
+age=19
+
+echo "My name is $name and my age is $age"
+
+name="Zoro"
+age=21
+echo "My name is $name and my age is $age"
+
+#Var to store the o/p of the commands
+HOSTNAME=$(hostname)
+PWD=$(pwd)
+
+echo "Name os the machine is $HOSTNAME"
+echo "Present working dir is : $PWD"
